2873fd817ecc5a42748a93802e69440aa62e5fd4
Bendegúz <bendeguz@medea.hu>
2021-09-26 16:22:29 +0200

    Festmeny.java hozzáadása

---
+++ src/hu/petrik/aukcioprojekt/Festmeny.java
@@ -0,0 +1,5 @@
+package hu.petrik.aukcioprojekt;
+
+public class Festmeny {
+
+}
--- src/hu/petrik/aukcioprojekt/Main.java
+++ src/hu/petrik/aukcioprojekt/Main.java
@@ -3,6 +3,6 @@ package hu.petrik.aukcioprojekt;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
