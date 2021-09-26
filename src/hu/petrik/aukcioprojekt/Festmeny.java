c3cbccc2938aa761a34d7ffc341cbdf4c68c348c
Bendegúz <bendeguz@medea.hu>
2021-09-26 16:37:36 +0200

    Festmeny get set

--- src/hu/petrik/aukcioprojekt/Festmeny.java
+++ src/hu/petrik/aukcioprojekt/Festmeny.java
@@ -8,16 +8,53 @@ public class Festmeny {
     private String stilus;
     private int licitekSzama;
     private int legmagasabbLicit;
-    private LocalDateTime legutolsoLicitIdeje ;
+    private LocalDateTime legutolsoLicitIdeje;
     private boolean elkelt;
 
     Festmeny(String cim, String festo, String stilus) {
         this.cim = cim;
         this.festo = festo;
         this.stilus = stilus;
+        this.licitekSzama = 0;
+        this.legmagasabbLicit = 0;
+        this.elkelt = false;
     }
 
+    public String getFesto() {
+        return this.festo;
+    }
 
+    public String getStilus() {
+        return this.stilus;
+    }
+
+    public int getLicitekSzama() {
+        return this.licitekSzama;
+    }
+
+    public int getLegmagasabbLicit() {
+        return this.legmagasabbLicit;
+    }
+
+    public LocalDateTime getLegutolsoLicitIdeje() {
+        return this.legutolsoLicitIdeje;
+    }
+
+    public boolean getElkelt() {
+        return this.elkelt;
+    }
+
+    public void setElkelt(boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+
+    public void licit() {
+
+    }
+
+    public void licit(int mertek) {
+
+    }
 
 
 
