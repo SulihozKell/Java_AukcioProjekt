cb260d60c18e3f0d6b8436df1a2aefd81d238807
Bendegúz <bendeguz@medea.hu>
2021-09-26 17:02:20 +0200

    Main-ben tesztelés

--- src/hu/petrik/aukcioprojekt/Main.java
+++ src/hu/petrik/aukcioprojekt/Main.java
@@ -3,6 +3,19 @@ package hu.petrik.aukcioprojekt;
 public class Main {
 
     public static void main(String[] args) {
-
+        Festmeny f1 = new Festmeny("Valami Cím", "Valaki Neve", "Stilusos");
+        f1.licit();
+        System.out.println(f1.getFesto());
+        System.out.println(f1.getLegutolsoLicitIdeje());
+        System.out.println(f1.getLicitekSzama());
+        System.out.println(f1.getLegmagasabbLicit());
+        f1.licit();
+        System.out.println(f1.getLegutolsoLicitIdeje());
+        System.out.println(f1.getLicitekSzama());
+        System.out.println(f1.getLegmagasabbLicit());
+        f1.licit();
+        System.out.println(f1.getLegutolsoLicitIdeje());
+        System.out.println(f1.getLicitekSzama());
+        System.out.println(f1.getLegmagasabbLicit());
     }
 }
