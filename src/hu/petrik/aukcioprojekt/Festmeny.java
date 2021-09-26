eafe9411d03181eddd73c1691210aa8eb11af83c
Bendegúz <bendeguz@medea.hu>
2021-09-26 16:28:45 +0200

    Festmeny alap létrehozása.

--- src/hu/petrik/aukcioprojekt/Festmeny.java
+++ src/hu/petrik/aukcioprojekt/Festmeny.java
@@ -1,5 +1,25 @@
 package hu.petrik.aukcioprojekt;
 
+import java.time.LocalDateTime;
+
 public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private int legmagasabbLicit;
+    private LocalDateTime legutolsoLicitIdeje ;
+    private boolean elkelt;
+
+    Festmeny(String cim, String festo, String stilus) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+    }
+
+
+
+
+
 
 }
